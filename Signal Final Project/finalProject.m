bc3cc1f0b54c7178dae9924ab72f6ef451c4c4ca
MohammadMostafa RostamKhani <mohammadmostafarostamkhani@gmail.com>
2021-01-26 18:25:10 +0330

    final

---
+++ Signal Final Project/finalProject.m
@@ -0,0 +1,10 @@
+recObj = audiorecorder;
+disp('Start speaking.');
+recordblocking(recObj, 5);
+disp('End of Recording.');
+play(recObj);
+y = getaudiodata(recObj);
+plot(y);
+Fs=1E+4;
+filename='myVoice.wav';
+audiowrite('myVoice.wav',y,Fs);
