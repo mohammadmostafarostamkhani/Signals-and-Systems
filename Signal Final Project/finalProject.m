ce234d58db1743b3503fcfd3a02539a66d97fe03
MohammadMostafa RostamKhani <mohammadmostafarostamkhani@gmail.com>
2021-01-27 16:44:40 +0330

    add noisy

--- Signal Final Project/finalProject.m
+++ Signal Final Project/finalProject.m
@@ -12,5 +12,21 @@ amp=0.5;
 delay=1;
 echo=echo_gen(y, Fs, delay, amp);
 audiowrite('myEchoVoice.wav',echo,Fs);
-noisy=y + randn(size(y));
-audiowrite('myNoisyVoice.wav',noisy,Fs);
+%noisy=y + randn(size(sqrt(y)));
+
+
+%STD=sqrt(0.005); % STANDARD DEVIATION
+
+%z=y+STD*randn(size(y));
+
+Mean=0.00;
+
+Var=0.0005;
+
+z=imnoise(y,'Gaussian',Mean,Var);
+
+
+
+audiowrite('myNoisyVoice.wav',z,Fs);
+
+
