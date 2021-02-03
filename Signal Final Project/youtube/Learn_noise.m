c0b08fd585ce74cfe6e724bc5ca21a1bdad89d08
Mohammadmostafa Rostamkhani <mohammadmostafarostamkhani@gmail.com>
2021-02-03 11:56:00 +0330

    final

--- Signal Final Project/youtube/Learn_noise.m
+++ Signal Final Project/youtube/Learn_noise.m
@@ -1,36 +1,35 @@
 [y1,fs] = audioread('myVoice.wav');
-X1 = y1 + 2*randn(length(y1),1); %Addintion of Noise using randn command
+X1 = y1 + 2*randn(length(y1),1);
 time=(1/fs)*length(y1);
-t=linspace(0,time,length(y1)) ;%For generating time-aixs
+t=linspace(0,time,length(y1)) ;
 subplot(5,1,1)
-plot(t,y1); title('Time domain plot of Orignal signal'); %This is original signal plot
+plot(t,y1); title('Time domain plot of Orignal signal');
 %plotting for noise added signal
 subplot(5,1,2)
 time1=(1/fs)*length(X1);
-t1=linspace(0,time1,length(X1)); %For generating time-axis
-plot(t1,X1); title('Time domain plot of Noise Added Siganl i.e X1.'); %This is plot will be of Noise added signal
+t1=linspace(0,time1,length(X1));
+plot(t1,X1); title('Time domain plot of Noise Added Siganl i.e X1.');
 audiowrite('noisy.wav',X1,fs);
-%Plotting original signal in frequency domain
 subplot(5,1,3)
-N=length(y1); %Determining length of signal
-Fc=(-N/2:N/2-1)/N; %For frequency normalizing axis
-F=fs*Fc; %generating frequency axis
+N=length(y1); 
+Fc=(-N/2:N/2-1)/N; 
+F=fs*Fc;
 we=fft(y1);
 we=fftshift(we);
-plot(F,we); title('Frequency plot of Orignal Signal'); %Plotting in frequency domain
+plot(F,we); title('Frequency plot of Orignal Signal');
 subplot(5,1,5)
 
 i=1;
-for j=2:length(X1)-1 %For averaging high frequency signal
-    y1(j,i) = (y1(j-1,i) + y1(j,i) + y1(j+1,i))/3 ; %simple average
+for j=2:length(X1)-1 
+    y1(j,i) = (y1(j-1,i) + y1(j,i) + y1(j+1,i))/3 ; 
 end
-g = gausswin(20); %Creataing Gaussian window of 20 with nuitin command
+g = gausswin(20); 
 g = g/sum(g); %It will be use for convolution
-y= conv(y1(:,1), g, 'same'); %Applying Convolution to remove effect of randn psudo no. added(noise)
-result=sgolayfilt(y,1,17); % Apply signal smoothing using Savitzky-Golay smoothing filter.
-audiowrite('result.wav',result,fs); % resultant signal can be write to the new file
-Nn=length(result); %length of new array which we got by manipulating org. signal
-Fn=(-Nn/2:Nn/2-1)/Nn; %Frequency PLOTING
+y= conv(y1(:,1), g, 'same');
+result=sgolayfilt(y,1,17); 
+audiowrite('result.wav',result,fs); 
+Nn=length(result); 
+Fn=(-Nn/2:Nn/2-1)/Nn; 
 Fq=fs*Fn;
 qa=fft(result);
 qa=fftshift(qa);
@@ -44,6 +43,4 @@ F1=Fa*fs;
 xa=fft(X1);
 xa=fftshift(X1);
 plot(F1,xa) ;title('Frequency plot of X1.');
-%Noise Removal Code
 subplot(5,1,5)
-%W=fft(X1);
